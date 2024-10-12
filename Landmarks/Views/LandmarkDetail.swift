ec08b4a72cb4f4734726d661689abed1146c9dd4
kiddo4 <olanrewajutaiwo183@gmail.com>
2024-10-12 07:50:34 -0700

    feat(fav): added favorites

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by kiddo   on 12/10/2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
