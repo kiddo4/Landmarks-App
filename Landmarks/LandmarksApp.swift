6380997dc6e2f2c2478cbbcad369d4a98b7606cd
kiddo4 <olanrewajutaiwo183@gmail.com>
2024-10-12 04:44:14 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by kiddo   on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
